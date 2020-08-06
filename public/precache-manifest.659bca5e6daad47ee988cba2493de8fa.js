8c37a7c22ba2ca0daa4b0f687e0dea503aeb468e
Axel Chauvin <axelchauvin@gmail.com>
2020-08-06 14:54:31 +0200

    Public folder commited

---
+++ public/precache-manifest.659bca5e6daad47ee988cba2493de8fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3de34d07a99735ff6741ab192742057",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7ac7c4bcd9d516c3170",
+    "url": "/static/css/main.1e388430.chunk.css"
+  },
+  {
+    "revision": "90e450cd2c5c35d19380",
+    "url": "/static/js/2.1adbc437.chunk.js"
+  },
+  {
+    "revision": "3da521dbb4874350fb1d5ccbd1bab881",
+    "url": "/static/js/2.1adbc437.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7ac7c4bcd9d516c3170",
+    "url": "/static/js/main.e7372427.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
